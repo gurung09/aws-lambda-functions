2369075e18a2bb651831c36f6fd0f7b12afb2f87
gurung09 <49125409+gurung09@users.noreply.github.com>
2022-03-23 11:43:46 -0400

    Update RefereshTrustedAdvisor.py

--- MonitorTrustedAdvisor/RefereshTrustedAdvisor.py
+++ MonitorTrustedAdvisor/RefereshTrustedAdvisor.py
@@ -6,11 +6,11 @@ from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
     try:
-        support_client = boto3.client('support', region_name='us-east-1')
-        ta_checks = support_client.describe_trusted_advisor_checks(language='en')
+        client = boto3.client('support', region_name='us-east-1')
+        ta_checks = client.describe_trusted_advisor_checks(language='en')
         for checks in ta_checks['checks']:
             try:
-                support_client.refresh_trusted_advisor_check(checkId=checks['id'])
+                client.refresh_trusted_advisor_check(checkId=checks['id'])
                 print('Refreshing check: ' + checks['name'])
             except ClientError:
                 print('Cannot refresh check: ' + checks['name'])
